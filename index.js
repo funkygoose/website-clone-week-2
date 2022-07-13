dc4294bd0295516b6aa8cd05694973b0f4d52933
Uriel Banuelos <uriel1119@gmail.com>
2022-07-13 10:28:44 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const navSlide = () => {
-   const burger = document.querySelector('.burger');
-   const nav = document.querySelector('.nav__links');
-
-   burger.addEventListener('click', () =>{
-      nav.classList.toggle('nav__active');
-   });
-}
-
-   navSlide();
+function openMenu() {
+   document.body.classList += " menu--open"
+ }
+ 
+ function closeMenu() {
+   document.body.classList.remove('menu--open')
+ }
